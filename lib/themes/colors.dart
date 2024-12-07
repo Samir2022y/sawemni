7b9bfea4d8bd934a0aac402f0b5ad796b542e9f5
lina-yahiaoui <130679757+lina-yahiaoui@users.noreply.github.com>
2024-12-07 09:31:14 +0100

    Update colors.dart

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -5,3 +5,6 @@ final informationColor = Color.lerp(primaryColor, Colors.white, 0.6)!;
 const signColor = Color(0xFFFE751A);
 const informationText = Color(0xFF989191);
 const backgroundColor = Color(0xFFFFFFFF);
+var orangeColor = const Color.fromARGB(252, 247, 157, 39);
+const secondorange = Color.fromARGB(255, 246, 138, 5);
+const grey = Color.fromARGB(255, 201, 198, 198);
